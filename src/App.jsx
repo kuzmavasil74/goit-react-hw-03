56cfb96b0a77e2464462d98eedec9fc228eb51f6
kuzmavasil74 <98240149+kuzmavasil74@users.noreply.github.com>
2024-03-22 09:47:36 +0100

    add_task-1

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
-import './App.module.css'
+import { useState } from 'react'
+import ContactForm from './components/ContactForm/ContactForm'
+import ContactList from './components/ContactList/ContactList'
+import SearchBox from './components/SearchBox/SearchBox'
+import contacts from './contacts.json'
+import Contact from './components/Contact/Contact'
 
 function App() {
   return (
     <div>
-      <h1>React template for home work</h1>
+      <h1>Phonebook</h1>
+      <ContactForm />
+      <SearchBox />
+      <ContactList contacts={contacts} />
+      <Contact />
     </div>
   )
 }
---
+++ src/components/ContactList/ContactList.jsx
@@ -0,0 +1,22 @@
+import css from './ContactList.module.css'
+
+const ContactList = ({ contacts }) => {
+  return (
+    <>
+      {contacts.map((contact) => {
+        return (
+          <div className={css.contactCart} key={contact.id}>
+            <ul className={css.contactList}>
+              <li className={css.contactListItem}>👨‍🦱{contact.name}</li>
+
+              <li className={css.contactListItem}>☎️{contact.number}</li>
+            </ul>
+            <button className={css.cantactListBtn}>Delete</button>
+          </div>
+        )
+      })}
+    </>
+  )
+}
+
+export default ContactList
